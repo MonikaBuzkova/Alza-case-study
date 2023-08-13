33e1aafe5240a6e0256fd31648b1db1926da10d5
Monika Buzkova <monika@buzkovi.cz>
2023-08-13 21:03:54 +0200

    almost finished

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-"use strict";
+// "use strict";
+
+// let zaskrolujAlzabox = document.querySelector(".scroll__Alzabox");
+// zaskrolujAlzabox.addEventListener("click", function () {
+//   zaskrolujAlzabox.scrollIntoView({
+//     // block: "start",
+//     behavior: "smooth",
+//     // inline: "start",
+//   });
+// });
+
+function scrollToSection(targetSectionId) {
+  var targetElement = document.getElementById(targetSectionId);
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  }
+}
